0d421e57bc944c9fd9cd81ba5953163c99920437
Benjamin Snowdon <bensnowdon@icloud.com>
2025-10-18 14:17:36 +0100

    Modified Mission Class

--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -75,8 +75,19 @@ class Mission:
 
     @classmethod
     def from_csv(cls, file_name: str):
-        # You are required to implement this method
-        pass
+        # Minimal implementation: load CSV directly with numpy and return Mission.
+        # The CSV is expected to have a header and three columns:
+        # reference, cave_height, cave_depth
+        data = np.loadtxt(file_name, delimiter=',', skiprows=1)
+
+        if data.ndim == 1:
+            data = data.reshape(1, -1)
+
+        reference = data[:, 0]
+        cave_height = data[:, 1]
+        cave_depth = data[:, 2]
+
+        return cls(reference, cave_height, cave_depth)
 
 
 class ClosedLoop:
